14049d8c502dd7ca1a1ddf946b5e519ef035c80b
Flora Jin <flora@pinterest.com>
2015-07-09 19:54:58 -0700

    fix thing + add gitignore

--- server/pinchats.py
+++ server/pinchats.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/flora/hackathon/pinchats/server/test.db'
+
 db = SQLAlchemy(app)
 
-pinchats_app = Flask(__name__)
-
 if __name__ == '__main__':
-    pinchats_app.run()
+    app.run()
