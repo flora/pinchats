e40bb58405432c83aac0c80ba87d7640b4a36fb0
Flora Jin <flora@pinterest.com>
2015-07-13 15:46:31 -0700

    talk to 0.0.0.0

--- server/pinchats.py
+++ server/pinchats.py
@@ -1,9 +1,16 @@
+import argparse
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+
+
+options = argparse.ArgumentParser()
+options.add_argument('-p', '--port', dest='port', default=9500,
+        type=int, help='the port to start the server on')
+args = options.parse_args()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/flora/hackathon/pinchats/test.db'
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=args.port)
